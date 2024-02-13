a159716e7f71caeefc729c43c27dd8033f24b748
rannn <rania.khedri@esprit.tn>
2024-02-13 15:26:55 +0100

    Crud book

--- src/main/java/tn/TheInformants/Entities/Book.java
+++ src/main/java/tn/TheInformants/Entities/Book.java
@@ -2,4 +2,93 @@ package tn.TheInformants.Entities;
 
 public class Book {
 
+    public Book() {
+
+    }
+
+    // Enum pour la disponibilité
+    public enum Disponibilite {
+        DISPO, NON_DISPO
+    }
+
+    // Enum pour la catégorie
+    public enum Categorie {
+        FICTION, NON_FICTION, SCIENCE_FICTION, MYSTERE, AUTRE
+    }
+
+    // Attributs de la classe Books
+    private int id_liv;
+    private String nom_liv;
+    private Disponibilite disponibilite;
+    private Categorie categorie;
+    private double prix_liv;
+
+    // Constructeur
+    public Book(int id_liv, String nom_liv, Disponibilite disponibilite, Categorie categorie, double prix_liv) {
+        this.id_liv = id_liv;
+        this.nom_liv = nom_liv;
+        this.disponibilite = disponibilite;
+        this.categorie = categorie;
+        this.prix_liv = prix_liv;
+    }
+    public Book( String nom_liv, Disponibilite disponibilite, Categorie categorie, double prix_liv) {
+
+        this.nom_liv = nom_liv;
+        this.disponibilite = disponibilite;
+        this.categorie = categorie;
+        this.prix_liv = prix_liv;
+    }
+
+    // Méthodes getters et setters
+
+    public int getId_liv() {
+        return id_liv;
+    }
+
+    public void setId_liv(int id_liv) {
+        this.id_liv = id_liv;
+    }
+
+    public String getNom_liv() {
+        return nom_liv;
+    }
+
+    public void setNom_liv(String nom_liv) {
+        this.nom_liv = nom_liv;
+    }
+
+    public Disponibilite getDisponibilite() {
+        return disponibilite;
+    }
+
+    public void setDisponibilite(Disponibilite disponibilite) {
+        this.disponibilite = disponibilite;
+    }
+
+    public Categorie getCategorie() {
+        return categorie;
+    }
+
+    public void setCategorie(Categorie categorie) {
+        this.categorie = categorie;
+    }
+
+    public double getPrix_liv() {
+        return prix_liv;
+    }
+
+    public void setPrix_liv(double prix_liv) {
+        this.prix_liv = prix_liv;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id_liv=" + id_liv +
+                ", nom_liv='" + nom_liv + '\'' +
+                ", disponibilite=" + disponibilite +
+                ", categorie=" + categorie +
+                ", prix_liv=" + prix_liv +
+                '}';
+    }
 }
---
+++ src/main/java/tn/TheInformants/Services/ServicesBook.java
@@ -0,0 +1,76 @@
+package tn.TheInformants.Services;
+
+import tn.TheInformants.Entities.Book;
+import tn.TheInformants.Utils.MyDataBase;
+
+import java.sql.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ServiceBook implements Iservices<Book> {
+    private final Connection connection;
+    public ServiceBook()
+    {
+        connection= MyDataBase.getInstance().getConnection();
+    }
+    @Override
+    public void ajouter(Book book) throws SQLException {
+        String sql = "INSERT INTO Books (id_liv, nom_liv, disponibilite_liv, categorie_liv, prix_liv) VALUES " +
+                "('" + book.getId_liv() + "','" + book.getNom_liv() + "','" +
+                book.getDisponibilite() + "','" + book.getCategorie() + "','" + book.getPrix_liv() + "')";
+
+        // Utilisation de PreparedStatement pour éviter les problèmes de sécurité avec les requêtes SQL
+        try (Statement statement = connection.createStatement()) {
+            statement.executeUpdate(sql);
+        }
+
+    }
+
+    @Override
+    public void modifier(Book book) throws SQLException {
+        String sql = "UPDATE Books SET id_liv = ?, Nom_liv = ?, Disponibilite_liv = ?, Categorie_liv = ?, Prix_liv = ? WHERE id_liv = ?";
+
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setInt(1, book.getId_liv());
+            preparedStatement.setString(2, book.getNom_liv());
+            preparedStatement.setString(3, book.getDisponibilite().toString());
+            preparedStatement.setString(4, book.getCategorie().toString());
+            preparedStatement.setDouble(5, book.getPrix_liv());
+
+
+            preparedStatement.executeUpdate();
+        }
+
+    }
+
+    @Override
+    public void supprimer(Book book) throws SQLException {
+        String sql="DELETE FROM books WHERE id_liv =?";
+        PreparedStatement preparedStatement=connection.prepareStatement(sql);
+        preparedStatement.setInt(1,book.getId_liv());
+        preparedStatement.executeUpdate();
+
+    }
+
+    @Override
+    public List<Book> recuperer() throws SQLException {
+        List<Book> booksList = new ArrayList<>();
+        String sql = "SELECT * FROM book";
+
+        try (Statement statement = connection.createStatement();
+             ResultSet rs = statement.executeQuery(sql)) {
+
+            while (rs.next()) {
+                Book book = new Book();
+                book.setId_liv(rs.getInt("id_liv"));
+                book.setNom_liv(rs.getString("Nom_liv"));
+                book.setDisponibilite(Book.Disponibilite.valueOf(rs.getString("Disponibilite_liv")));
+                book.setCategorie(Book.Categorie.valueOf(rs.getString("Categorie_liv")));
+                book.setPrix_liv(rs.getDouble("Prix_liv"));
+                booksList.add(book);
+            }
+        }
+        return booksList;
+    }
+}
